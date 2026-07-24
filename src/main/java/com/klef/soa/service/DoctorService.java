d9c0cd0843982d42d13aa4948c8e58d49a837d9a
venkata sanjeev <jagansanjeev2007@gmail.com>
2026-07-24 08:48:23 +0530

    Added Controller and Service

--- src/main/java/com/klef/soa/service/DoctorService.java
+++ src/main/java/com/klef/soa/service/DoctorService.java
@@ -1,7 +1,6 @@
 package com.klef.soa.service;
 
 import java.util.List;
-
 import com.klef.soa.entity.Doctor;
 
 public interface DoctorService 
@@ -9,5 +8,7 @@ public interface DoctorService
 	Doctor addDoctor(Doctor d);
 	Doctor updateDoctor(Doctor d);
 	List<Doctor> displayAllDoctors();
-	
+	Doctor displayDoctorById(Long id);
+	String deleteDoctorById(Long id);
+	List<Doctor> displayDoctorsByGender(String gender);
 }
