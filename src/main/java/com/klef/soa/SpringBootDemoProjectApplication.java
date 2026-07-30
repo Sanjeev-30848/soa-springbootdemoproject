72cc5709c3635aed4a4989480599636c86c8acdb
venkata sanjeev <jagansanjeev2007@gmail.com>
2026-07-30 17:10:52 +0530

    implemented rest template

--- src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
+++ src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
@@ -2,6 +2,8 @@ package com.klef.soa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootDemoProjectApplication 
@@ -13,4 +15,9 @@ public class SpringBootDemoProjectApplication
 		System.out.println("Project is running...!!!");
 	}
 
+	@Bean
+	public RestTemplate restTemplate()
+	{
+        return new RestTemplate();
+	}
 }
